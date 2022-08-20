e3c1b63a1d92514e54b0a0ff55ffbe5770498866
Arpit Shukla <arpitshukla62@gmail.com>
2022-08-20 18:05:01 +0530

    Write deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,36 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy")
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 module.exports = {
-  solidity: "0.8.9"
+    networks: {
+        hardhat: {
+            chainId: 31337
+        },
+        localhost: {
+            chainId: 31337,
+            url: 'http://127.0.0.1:8545/'
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+            gas: 6000000
+        }
+    },
+    solidity: "0.8.9",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0
+        }
+    }
+
 };
---
+++ helper-hardhat-config.js
@@ -0,0 +1,26 @@
+const networkConfig = {
+    31337: {
+        name: "localhost",
+        subscriptionId: "588", // Any value here
+        keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        keepersUpdateInterval: "30",
+        lotteryEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000"
+    },
+    4: {
+        name: "rinkeby",
+        subscriptionId: "6926",
+        keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        keepersUpdateInterval: "30",
+        lotteryEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000",
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains
+}
